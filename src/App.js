dff4679913eec6202f75b636c6aa5afbb7915489
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2021-02-11 22:39:38 -0500

    links work

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class  App extends React.Component {
     <Header />
     <Bar />
     <AboutMe />
+    
     <PortfolioItems />
     </div>
   )
--- src/components/portfolioItems.js
+++ src/components/portfolioItems.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Github} from '@styled-icons/boxicons-logos/Github';
+
 import {portfolioContent} from './portfolioContent';
 import DevIcon from "devicon-react-svg";
 
 const PortfolioContainer = styled.body`
 background: #F4F4ED;
+display: flex;
+flex-flow: row wrap;
+align-content: flex-start;
+position: relative;
+justify-content: center;
+aling-items: center;
+width: 100%;
+margin-left:  auto;
+margin-right: auto;
+
 `;
 
 const CardWrapper = styled.div`
 width: 250px;
 height: 250px;
-border: solid #031D44;
+border: solid #124E78;
 border-radius: 5px;
+margin: 15px;
+color: #404E5C;
+position: relative;
+display: block;
+justify-content: center;
+aling-items: center;
+margin-left:  auto;
+margin-right: auto;
 ;
 
 
 `;
 
 const Iconheader = styled.header`
-background: #72DDF7;
+background: #F4F4ED;
 height: 45px;
 width: 100%;
-color: #3B3C4A
+color: #FE4A49;
 
 `;
 
@@ -46,20 +64,25 @@ const PortfolioItems = () => {
             portfolioContent.map((item) =>       
             <CardWrapper>
                 <Iconheader>
-                <p>{item.title}</p>
-                <StyledIcon icon={item.css} />
-                <StyledIcon icon={item.javascript}/>
-                <StyledIcon icon={item.html} />
-                <StyledIcon icon={item.node} />
-                <StyledIcon icon={item.react} />
-                <StyledIcon icon={item.jquery} />
-                <StyledIcon icon={item.mongo} />
-                <StyledIcon icon={item.mysql} />
-                <StyledIcon icon={item.bootstrap}/>
-                <StyledIcon icon={item.npm} />
-                    
+                    <StyledIcon icon={item.css} />
+                    <StyledIcon icon={item.javascript}/>
+                    <StyledIcon icon={item.html} />
+                    <StyledIcon icon={item.node} />
+                    <StyledIcon icon={item.react} />
+                    <StyledIcon icon={item.jquery} />
+                    <StyledIcon icon={item.mongo} />
+                    <StyledIcon icon={item.mysql} />
+                    <StyledIcon icon={item.bootstrap}/>
+                    <StyledIcon icon={item.npm} />
+                    <a style={{color: '#FE4A49'}}href={item.url} target="_blank" rel="noopener noreferrer">
+                    <h3>{item.title}</h3>
+                    </a>
                     
                 </Iconheader>
+                <p>{item.description}</p>
+                    <a href={item.github} target="_blank" rel="noopener noreferrer">
+                        <StyledIcon icon="github_badge" />
+                    </a>
             </CardWrapper>
             
             
